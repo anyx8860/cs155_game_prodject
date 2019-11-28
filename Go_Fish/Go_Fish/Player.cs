252c9e78c172f89d6b7efad3b9232cc581112690
Alicia Stanton <atkelly013113@gmail.com>
2019-11-28 09:43:24 -0800

    Erased secondary main method from Player Class

--- Go_Fish/Go_Fish/Player.cs
+++ Go_Fish/Go_Fish/Player.cs
@@ -1,76 +1,102 @@
-﻿
-        using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Go_Fish
+{
+    class Player
     {
-        class MainClass
+        private Random random = new Random();
+        // What kind of information do we need to have for 
+        // each player? - The instance variables
+
+        private string playerName;
+        public Deck cardDeck { get; set; }
+
+
+
+        private List<Card> PlayerHand;
+
+        public List<Card> getPlayerActualHand()
         {
-            public static void Main(string[] args)
-            {
-                Console.WriteLine("Hello World");
-                // Create the deck of cards for the Go_Fish Game
-                Deck gameDeck = new Deck();
-
-                // Print each card in the deck to make sure that
-                // all cards are present - should be 52 total
-                foreach (Card cardOut in gameDeck.getCardDeck())
-                {
-                    Console.WriteLine(cardOut.toString());
-                    Console.WriteLine();
-                }
-
-                // Get the player's hand - should be seven random cards
-                // from the original deck
-                List<Card> player1Hand;
-                player1Hand = gameDeck.getPlayerHand();
-
-                foreach (Card playerCard in player1Hand)
-                {
-                    Console.WriteLine(playerCard.toString());
-                }
-
-                // Check how many cards are in the gameDeck - 
-                // we removed 7 cards for the first player, should be
-                // 45 cards left
-                Console.WriteLine();
-                Console.WriteLine("The number of cards in the deck now are: {0}", gameDeck.getCount());
-                Console.WriteLine();
-
-
-                // Create a Second hand of cards for a potential
-                // second player
-                Player player2 = new Player();
-                List<Card> player2Hand;
-                player2Hand = gameDeck.getPlayerHand();
-
-                foreach (Card player2Card in player2Hand)
-                {
-                    Console.WriteLine(player2Card.toString());
-                }
-
-
-                // Make sure that the cards were removed from the deck
-                // properly by checking how many cards are currently in
-                // the deck - this should return 38
-
-                Console.WriteLine();
-                Console.WriteLine("The current number of cards in the deck is: {0}", gameDeck.getCount());
-
-
-                // Test the peek method, if we were not printing out the cards to the console - theoretical
-
-
-
-
-
-
-
-            }
+            this.PlayerHand = cardDeck.getPlayerHand();
+            return PlayerHand;
         }
+        // Instance variable for number of Books
+
+
+        // Empty Constructor
+        public Player()
+        {
+            this.playerName = "Anonymous";
+        }
+
+        // Full Constructor
+        public Player(string nameIn)
+        {
+            this.playerName = nameIn;
+        }
+
+        // Copy Constructor
+        public Player(Player theObject)
+        {
+            this.playerName = theObject.getPlayerName();
+        }
+
+        // Getter and Setter methods
+        public string getPlayerName()
+        {
+            return this.playerName;
+        }
+
+        public void setPlayerName()
+        {
+            Console.Write("Please enter your name: ");
+            this.playerName = Console.ReadLine();
+        }
+
+        public void setDeck(Deck cardDeck)
+        {
+            this.cardDeck = cardDeck;
+        }
+
+        public Deck getDeck()
+        {
+            return this.cardDeck;
+        }
+
+
+
+
+
+        // Equals methods
+
+
+        // To String method
+        public string toString()
+        {
+            return string.Format("Player Name: {0}", this.playerName);
+        }
+
+
+        // Method to ask for a card?
+
+
+        // Method so each player can peek at their cards?
+        public void Peek(List<Card> playerHandIn)
+        {
+            foreach (Card handCard in playerHandIn)
+            {
+                Console.WriteLine(handCard.toString());
+            }
+            // Ask which card they would like to pick to ask 
+            // their opponent, then call the ask for card method
+            Console.Write("Which card would you like to pick?: ");
+        }
+
     }
+}
 
 
